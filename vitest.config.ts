f35379018f5fa9c7095789e7fbdfbf665c5fd837
Michal Lata <michallata.dev@gmail.com>
2024-01-26 20:20:30 +0100

    chore: Update configs

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,13 +1,14 @@
+/* eslint-disable import/no-extraneous-dependencies */
 import { defineConfig } from "vitest/config";
 import react from "@vitejs/plugin-react";
 import { resolve } from "path";
 
 export default defineConfig({
-  plugins: [react()],
-  test: {
-    environment: "jsdom"
-  },
-  resolve: {
-    alias: [{ find: "@", replacement: resolve(__dirname, "./src") }]
-  }
+    plugins: [react()],
+    test: {
+        environment: "jsdom",
+    },
+    resolve: {
+        alias: [{ find: "@", replacement: resolve(__dirname, "./src") }],
+    },
 });
