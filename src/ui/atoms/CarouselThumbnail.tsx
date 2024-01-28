30e45e92bb669a90ccd1862a65ac9ef0461f56a4
Michal Lata <michallata.dev@gmail.com>
2024-01-28 20:14:42 +0100

    feat: Add CarouselThumbnail component

---
+++ src/ui/atoms/CarouselThumbnail.tsx
@@ -0,0 +1,26 @@
+import { TPhoto } from "@/ui/types";
+import Image from "next/image";
+
+type TPhotoProps = {
+    photo: TPhoto;
+    setOpenedPhoto: (photo: TPhoto) => void;
+    openedPhotoId: number;
+};
+
+export default function CarouselThumbnail({
+    photo,
+    setOpenedPhoto,
+    openedPhotoId,
+}: TPhotoProps) {
+    return (
+        <Image
+            width={150}
+            height={100}
+            src={photo.src}
+            alt={photo.alt}
+            onClick={() => setOpenedPhoto(photo)}
+            className={`h-full w-full rounded-sm object-cover ${openedPhotoId === photo.id ? "box-border border-4" : ""}`}
+            data-click-outside="true"
+        />
+    );
+}
