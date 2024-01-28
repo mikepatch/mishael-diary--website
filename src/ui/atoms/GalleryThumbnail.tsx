9a003af5c9f60c47cb8a3ec551f32d5bf6faa776
Michal Lata <michallata.dev@gmail.com>
2024-01-28 20:14:18 +0100

    feat: Add GalleryThumbnail component

---
+++ src/ui/atoms/GalleryThumbnail.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+import Image from "next/image";
+import { TPhoto } from "../types";
+
+type TGalleryThumbnailProps = {
+    photo: TPhoto;
+    setOpenedPhoto: (photoToOpen: TPhoto) => void;
+};
+
+export default function GalleryThumbnail({
+    photo,
+    setOpenedPhoto,
+}: TGalleryThumbnailProps) {
+    return (
+        <figure className=" w-full cursor-pointer overflow-hidden">
+            <Image
+                src={photo.src}
+                alt={photo.alt}
+                width={300}
+                height={300}
+                onClick={() => setOpenedPhoto(photo)}
+                className="aspect-square h-full w-full cursor-zoom-in rounded-sm object-cover shadow-sm brightness-90 transition-all hover:brightness-100"
+                blurDataURL="data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw=="
+                placeholder="blur"
+            />
+        </figure>
+    );
+}
