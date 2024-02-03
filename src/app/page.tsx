fd519dc83eea17e89f5a7d146bd12d40740a6237
Michal Lata <michallata.dev@gmail.com>
2024-02-03 18:40:42 +0100

    patch: Update HomePage component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,9 @@
 import IntroHeader from "@/ui/molecules/IntroHeader";
 import IntroNav from "@/ui/molecules/IntroNav";
 
-export default function Home() {
+export default function HomePage() {
     return (
-        <section className="flex h-screen flex-col items-center justify-center gap-10 bg-gradient-to-tl from-brand-background to-brand-background-light">
+        <section className="flex h-screen flex-col items-center justify-center gap-10 ">
             <IntroHeader />
             <IntroNav />
         </section>
