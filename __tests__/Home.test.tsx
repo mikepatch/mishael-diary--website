6f253dbb4afbb8c91534f6087e2e9ad08ed6bb8e
Michal Lata <michallata.dev@gmail.com>
2024-02-03 18:39:38 +0100

    patch: Update Home test

--- __tests__/Home.test.tsx
+++ __tests__/Home.test.tsx
@@ -1,12 +1,12 @@
-import { describe, expect, it, test } from "vitest";
+import { describe, expect, it } from "vitest";
 import { render, screen } from "@testing-library/react";
 import Home from "@/app/page";
 
 describe("HomePage", () => {
-  it("should heading 1 be defined", () => {
-    render(<Home />);
-    expect(
-      screen.getByRole("heading", { level: 1, name: "Home" })
-    ).toBeDefined();
-  });
+    it("should render HomePage", () => {
+        render(<Home />);
+        expect(
+            screen.getByRole("heading", { level: 1, name: "Mishael Diary" })
+        ).toBeDefined();
+    });
 });
