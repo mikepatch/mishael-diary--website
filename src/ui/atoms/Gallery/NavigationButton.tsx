87de6b68fa202be7c950466f01db8f2142b02481
Michal Lata <michallata.dev@gmail.com>
2024-02-03 18:44:49 +0100

    feat: Add NavigationButton component for Carousel

---
+++ src/ui/atoms/Gallery/NavigationButton.tsx
@@ -0,0 +1,34 @@
+import { FaChevronLeft, FaChevronRight } from "react-icons/fa6";
+
+type TDirection = "right" | "left";
+
+type TNavigationButtonProps = {
+    direction: TDirection;
+    handleSwipe: (direction: TDirection) => void;
+};
+
+export default function NavigationButton({
+    direction,
+    handleSwipe,
+}: TNavigationButtonProps) {
+    return (
+        <button
+            aria-label={`Navigate ${direction}`}
+            type="button"
+            onClick={() => handleSwipe(direction)}
+            className={`group absolute ${direction === "left" ? "left-0" : "right-0"} top-0 h-full px-6 text-2xl text-white transition-all lg:px-16`}
+            data-click-outside="true"
+        >
+            <div className="rounded-full bg-white bg-opacity-10 p-4 transition-all group-hover:scale-105 group-hover:bg-opacity-20">
+                {direction === "left" ? (
+                    <FaChevronLeft
+                        className="bg-opacity-0"
+                        data-click-outside="true"
+                    />
+                ) : (
+                    <FaChevronRight />
+                )}
+            </div>
+        </button>
+    );
+}
