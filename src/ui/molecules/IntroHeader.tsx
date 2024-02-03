f28dd0f38b1be00c553eca173a09d5b80bd3c20f
Michal Lata <michallata.dev@gmail.com>
2024-02-03 18:37:00 +0100

    patch: Update IntroHeader component

--- src/ui/molecules/IntroHeader.tsx
+++ src/ui/molecules/IntroHeader.tsx
@@ -3,14 +3,14 @@ import Image from "next/image";
 export default function IntroHeader() {
     return (
         <header>
-            <figure className="relative aspect-square w-40 overflow-hidden rounded-[50%] border border-black">
+            <figure className="relative aspect-square w-40 overflow-hidden rounded-[50%] border-2 border-emerald-500">
                 <Image
                     src="/img/mishael-avatar.jpeg"
                     width={200}
                     height={200}
                     alt="Awatar Mishael"
                     loading="eager"
-                    blurDataURL="data:image/png;base64, iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAADUlEQVR42mNkYPhfDwAChwGA60e6kgAAAABJRU5ErkJggg=="
+                    blurDataURL="data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw=="
                     placeholder="blur"
                 />
             </figure>
