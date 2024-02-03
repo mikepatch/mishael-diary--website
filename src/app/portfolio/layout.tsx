a2e415bf10f28fedc60e9cbf1cb7e1b41504c5dd
Michal Lata <michallata.dev@gmail.com>
2024-02-03 18:42:18 +0100

    feat: Add PortfolioLayout component

---
+++ src/app/portfolio/layout.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+import SubPageHeader from "@/ui/molecules/SubPageHeader";
+
+export default function PortfolioLayout({
+    children,
+}: Readonly<{
+    children: React.ReactNode;
+}>) {
+    return (
+        <>
+            <SubPageHeader title="Portfolio." />
+            <main className="px-6 sm:px-14">{children}</main>
+        </>
+    );
+}
