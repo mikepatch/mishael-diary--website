d72c90f1fa55307bff12b47200b7cba88c2cbe4f
Michal Lata <michallata.dev@gmail.com>
2024-02-04 14:39:30 +0100

    feat: Add Photo component

---
+++ src/ui/atoms/Gallery/Photo.tsx
@@ -0,0 +1,20 @@
+import Image from "next/image";
+import { TPhoto } from "../../types";
+
+type TPhotoProps = {
+    photo: TPhoto;
+};
+
+export default function Photo({ photo }: TPhotoProps) {
+    return (
+        <Image
+            src={photo.src}
+            alt={photo.alt}
+            width={400}
+            height={500}
+            className="h-full w-fit cursor-auto rounded-sm object-contain"
+            blurDataURL="data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw=="
+            placeholder="blur"
+        />
+    );
+}
