6702ac8b6b0079bb17cdc921430aa2a721d25307
Michal Lata <michallata.dev@gmail.com>
2024-02-04 15:00:36 +0100

    patch: Add viewport settings in layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import React from "react";
 import { Inter } from "next/font/google";
 
 import "./globals.css";
+import Head from "next/head";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -18,6 +19,12 @@ export default function RootLayout({
 }>) {
     return (
         <html lang="pl">
+            <Head>
+                <meta
+                    name="viewport"
+                    content="width=device-width, initial-scale=1.0"
+                />
+            </Head>
             <body
                 className={`${inter.className} mx-auto min-h-screen overflow-x-hidden bg-gradient-to-tl from-brand-background to-brand-background-light`}
             >
